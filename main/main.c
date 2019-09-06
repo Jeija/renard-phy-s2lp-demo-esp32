1aa2c5c22956124400454d5b813474eae9e93ce2
Jeija <norrepli@gmail.com>
2019-09-06 22:06:08 +0200

    Store sequence number in NVS

--- main/main.c
+++ main/main.c
@@ -5,23 +5,41 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_sleep.h"
+#include "nvs_flash.h"
+#include "nvs.h"
 
 #include "renard_phy_s2lp_protocol.h"
 #include "renard_phy_s2lp.h"
 
-/*void renard_phy_s2lp_hal_init(void);
-void renard_phy_s2lp_hal_spi(uint8_t length, uint8_t *in, uint8_t *out);
-void renard_phy_s2lp_hal_shutdown(bool shutdown);
-void renard_phy_s2lp_hal_interrupt_timeout(uint32_t milliseconds);
-void renard_phy_s2lp_hal_interrupt_gpio(bool risingTrigger);
-void renard_phy_s2lp_hal_interrupt_clear(void);
-int renard_phy_s2lp_hal_interrupt_wait(void);*/
+/* ***PUT YOUR SIGFOX CREDENTIALS HERE*** */
+uint8_t key[] = {0x47, 0x9e, 0x44, 0x80, 0xfd, 0x75, 0x96, 0xd4, 0x5b, 0x01, 0x22, 0xfd, 0x28, 0x2d, 0xb3, 0xcf};
+uint32_t devid = 0x004d33db;
+uint8_t payload[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x12, 0x34, 0x56};
 
-void renard_phy_s2lp_hal_interrupt_timeout(uint32_t milliseconds);
-int renard_phy_s2lp_hal_interrupt_wait(void);
+static uint16_t next_seqnum(void)
+{
+    /* Increment sequence number: Load current value from NVS, increment and store updated value */
+    esp_err_t err = nvs_flash_init();
+    if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ESP_ERROR_CHECK(nvs_flash_init());
+    } else {
+		ESP_ERROR_CHECK(err);
+	}
+
+	nvs_handle_t nvshandle;
+	ESP_ERROR_CHECK(nvs_open("sigfox", NVS_READWRITE, &nvshandle));
+	uint16_t seqnum = 0;
+	nvs_get_u16(nvshandle, "seqnum", &seqnum);
+	seqnum = (seqnum + 1) % 0xfff;
+	nvs_set_u16(nvshandle, "seqnum", seqnum);
+
+	return seqnum;
+}
 
 void app_main(void)
 {
+	/* S2-LP SPI communication initialization */
 	printf("[renard-phy-s2lp-demo-esp32] Initializing S2-LP...\r\n");
 
 	if (renard_phy_s2lp_init()) {
@@ -31,23 +49,17 @@ void app_main(void)
 		vTaskDelay(1000 / portTICK_PERIOD_MS);
 		esp_restart();
 	}
-	renard_phy_s2lp_protocol_init(1234, UL_DATARATE_100BPS);
+	renard_phy_s2lp_protocol_init(esp_random(), UL_DATARATE_100BPS);
+
 	vTaskDelay(100 / portTICK_PERIOD_MS);
 
-	/* Pycom SiPy credentials */
-	uint8_t key[] = {0x47, 0x9e, 0x44, 0x80, 0xfd, 0x75, 0x96, 0xd4, 0x5b, 0x01, 0x22, 0xfd, 0x28, 0x2d, 0xb3, 0xcf};
-	uint32_t devid = 0x004d33db;
-	uint8_t payload[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x44, 0x11, 0x33};
-
+	/* Prepare uplink */
+	printf("[renard-phy-s2lp-demo-esp32] Starting message transfer!\r\n");
 	sfx_commoninfo common;
-	common.seqnum = 1234;
+	common.seqnum = next_seqnum();
 	common.devid = devid;
 	memcpy(common.key, key, 16);
 
-	printf("[renard-phy-s2lp-demo-esp32] Starting message transfer!\r\n");
-	vTaskDelay(100 / portTICK_PERIOD_MS);
-
-	/* Prepare payload */
 	sfx_ul_plain uplink;
 	memcpy(uplink.payload, payload, sizeof(payload));
 	uplink.payloadlen = sizeof(payload);
@@ -55,20 +67,26 @@ void app_main(void)
 	uplink.singlebit = false;
 	uplink.replicas = true;
 
+	/* Actual protocol operation */
 	sfx_dl_plain downlink;
 	int16_t downlink_rssi;
 	int err = renard_phy_s2lp_protocol_transfer(&common, &uplink, &downlink, &downlink_rssi);
 
+	/* Log result */
 	if (err == PROTOCOL_ERROR_NONE) {
-		printf("[renard-phy-s2lp-demo-esp32] Downlink received!\r\n");
-		printf("[renard-phy-s2lp-demo-esp32] Downlink RSSI    : %d\r\n", downlink_rssi);
-		printf("[renard-phy-s2lp-demo-esp32] Downlink payload : ");
-		for (int i = 0; i < SFX_DL_PAYLOADLEN; i++)
-			printf("%02x", downlink.payload[i]);
-		printf("\r\n");
-		printf("[renard-phy-s2lp-demo-esp32] Downlink CRC OK  : %d\r\n", downlink.crc_ok);
-		printf("[renard-phy-s2lp-demo-esp32] Downlink MAC OK  : %d\r\n", downlink.mac_ok);
-		printf("[renard-phy-s2lp-demo-esp32] Downlink FEC Use : %d\r\n", downlink.fec_corrected);
+		if (uplink.request_downlink) {
+			printf("[renard-phy-s2lp-demo-esp32] Downlink received!\r\n");
+			printf("[renard-phy-s2lp-demo-esp32] Downlink RSSI    : %d\r\n", downlink_rssi);
+			printf("[renard-phy-s2lp-demo-esp32] Downlink payload : ");
+			for (int i = 0; i < SFX_DL_PAYLOADLEN; i++)
+				printf("%02x", downlink.payload[i]);
+			printf("\r\n");
+			printf("[renard-phy-s2lp-demo-esp32] Downlink CRC OK  : %d\r\n", downlink.crc_ok);
+			printf("[renard-phy-s2lp-demo-esp32] Downlink MAC OK  : %d\r\n", downlink.mac_ok);
+			printf("[renard-phy-s2lp-demo-esp32] Downlink FEC Use : %d\r\n", downlink.fec_corrected);
+		} else {
+			printf("[renard-phy-s2lp-demo-esp32] Uplink transmitted, no downlink requested.");
+		}
 	} else if (err == PROTOCOL_ERROR_TIMEOUT) {
 		printf("[renard-phy-s2lp-demo-esp32] Timeout while waiting for downlink\r\n");
 	} else {
